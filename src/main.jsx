46456a8860e0d82f6fb80deca06107f72e8d66bd
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-12-05 02:14:12 +1300

    add todo

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
+// TODO: rewrite to typescript
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
