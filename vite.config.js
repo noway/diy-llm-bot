784f378321c960e823c0b15b3a7875245b1c2e62
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-12-05 23:01:05 +1300

    use https in dev

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import fs from 'fs';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  server: {
+    https: {
+      key: fs.readFileSync('./localhost-key.pem'),
+      cert: fs.readFileSync('./localhost.pem'),
+    },
+  },
   plugins: [react()]
 })
