79c9260ace5ae51dc8a667b24cb28ad5b94227d9
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-12-05 23:18:19 +1300

    migrate to react-ts template

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 
-// TODO: rewrite to typescript
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
