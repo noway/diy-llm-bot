1bc72e3449a77454078a3b8cbec651560c2ce0e6
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2022-12-05 23:36:46 +1300

    fix development server not working

--- vite.config.ts
+++ vite.config.ts
@@ -2,13 +2,18 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import fs from "fs";
 
+const NODE_ENV = process.env.NODE_ENV ?? "development";
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: process.env.NODE_ENV === 'development' ? {
-    https: {
-      key: fs.readFileSync("./localhost-key.pem"),
-      cert: fs.readFileSync("./localhost.pem"),
-    },
-  } : {},
+  server:
+    NODE_ENV === "development"
+      ? {
+          https: {
+            key: fs.readFileSync("./localhost-key.pem"),
+            cert: fs.readFileSync("./localhost.pem"),
+          },
+        }
+      : {},
   plugins: [react()],
 });
