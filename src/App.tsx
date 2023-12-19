da1f9712d48e7b12bde87b2a1a67f7bd6ffe1d47
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2023-12-18 21:24:02 -0500

    new model

--- src/App.tsx
+++ src/App.tsx
@@ -587,6 +587,7 @@ function App() {
                 <option value="text-davinci-003">text-davinci-003</option>
                 <option value="gpt-3.5-turbo">gpt-3.5-turbo</option>
                 <option value="gpt-4-1106-preview">gpt-4-1106-preview</option>
+                <option value="mistralai/Mixtral-8x7B-Instruct-v0.1">mistralai/Mixtral-8x7B-Instruct-v0.1</option>
                 {authKey ? (
                   <option value="gpt-4">gpt-4</option>
                 ) : null}
