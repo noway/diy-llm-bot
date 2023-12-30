5259acd39c6aec49f0624526a96833c4f9be31d3
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2023-12-30 02:49:03 -0500

    fix cursor extending horizontal scroll

--- src/App.tsx
+++ src/App.tsx
@@ -240,7 +240,7 @@ function CodeBlock(props: { lineCount: number, nodeLineCount: number, blink: boo
             key: index,
           }));
           if (isLastParagraph && blink) {
-            elements.push(<span className="blinking-cursor blinking-cursor--code" key="blinking-cursor" />);
+            elements.push(<span className="blinking-cursor--container"><span className="blinking-cursor blinking-cursor--code" key="blinking-cursor" /></span>);
           }
           return elements;
         }}
