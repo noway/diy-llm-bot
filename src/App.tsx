3e78e90815e673a348bbdab4aa6482be61660f55
Ilia Sidorenko <ilia.sidorenko.421@gmail.com>
2024-07-19 01:38:35 -0400

    add gpt-4o-mini

--- src/App.tsx
+++ src/App.tsx
@@ -644,6 +644,7 @@ function App() {
                 <option value="gpt-3.5-turbo">gpt-3.5-turbo</option>
                 <option value="gpt-4-1106-preview">gpt-4-1106-preview</option>
                 <option value="gpt-4o">gpt-4o</option>
+                <option value="gpt-4o-mini">gpt-4o-mini</option>
                 <option value="anthropic/claude-3-opus:beta">anthropic/claude-3-opus:beta</option>
                 <option value="mistralai/Mixtral-8x7B-Instruct-v0.1">mistralai/Mixtral-8x7B-Instruct-v0.1</option>
                 <option value="mistralai/mistral-large">mistralai/mistral-large</option>
